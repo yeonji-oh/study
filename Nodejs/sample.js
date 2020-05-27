e49acaa4595ae04f660e48abee4a0f24e8f01a52
yeonji.oh <yeonji20@naver.com>
2020-05-27 20:35:28 +0900

    20200527

--- Nodejs/sample.js
+++ Nodejs/sample.js
@@ -7,6 +7,13 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+
+  var accountStr = '{"name":"Jedi", "members":["Yoda", "Obi Wan"], "number":34512, "location":"A galaxy far, far away"}';
+  var accountObj = JSON.parse(accountStr);
+  var aaa = JSON.stringify(accountObj, null, 2);
+
+  console.log(aaa);       //Jedi
+  //console.log("eee" + accountObj.members);
 });
 
 server.listen(port, hostname, () => {
